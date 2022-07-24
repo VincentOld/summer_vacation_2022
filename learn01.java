40963c092cd39a31f7e5fbdceb6fad68bcd1d728
VincentOld <1367342791@qq.com>
2022-07-24 21:38:18 +0800

    IDEA编辑器学习笔记01

---
+++ learn01.java
@@ -0,0 +1,8 @@
+package summer.vacation_01;
+import java.util.ArrayList;
+public class learn01 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        ArrayList list = new Arr;
+    }
+}
